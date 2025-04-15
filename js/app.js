72a5664a74f03401bd450c2b7dbd0e345a350ac4
Josh Kelly <jkelly85@alumni.uwo.ca>
2025-04-15 14:09:00 -0400

    add set object prop to fullName funnc

--- js/app.js
+++ js/app.js
@@ -14,12 +14,30 @@ const app = new Vue({
 
     // fullName func - returns formatted name
     fullName: {
+      
       get: function() {
+
         if (this.firstName && this.lastName)
+
         {
           return this.firstName + ' ' + this.lastName;
+
         } else {
           return this.firstName || this.lastName;
+        }
+      },
+
+      set: function(newFullName) {
+        const names = newFullName.split(' ');
+
+        if (names.length === 2) {
+          this.firstName = names[0];
+          this.lastName = names[1];
+        }
+
+        if (names.length <= 1) {
+          this.firstName = names[0] || '';
+          this.lastName = '';
         }
       }
     },
